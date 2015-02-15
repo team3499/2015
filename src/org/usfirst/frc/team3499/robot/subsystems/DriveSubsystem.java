6c734d831569edf1a6cdddda331fd247af84d201
bturnbull <github@brianturnbull.com>
2015-02-15 14:17:31 -0500

    Off bench testing on Talon SRX
    Change all CANTalon speed controllers back to PWM Jaguars.

--- src/org/usfirst/frc/team3499/robot/subsystems/DriveSubsystem.java
+++ src/org/usfirst/frc/team3499/robot/subsystems/DriveSubsystem.java
@@ -1,8 +1,8 @@
 package org.usfirst.frc.team3499.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.CANTalon;
-import edu.wpi.first.wpilibj.CANTalon.ControlMode;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Jaguar;
 
 import org.usfirst.frc.team3499.robot.RobotMap;
 import org.usfirst.frc.team3499.robot.Drive;
@@ -15,32 +15,34 @@ import org.usfirst.frc.team3499.robot.commands.DriveTeleopCommand;
  */
 public class DriveSubsystem extends Subsystem {
 
-    // Jaguar motor1 = new Jaguar(RobotMap.driveMotorLFPort);
-    // Jaguar motor2 = new Jaguar(RobotMap.driveMotorLRPort);
-    // Jaguar motor3 = new Jaguar(RobotMap.driveMotorRFPort);
-    // Jaguar motor4 = new Jaguar(RobotMap.driveMotorRRPort);
+    Jaguar[] motors = { new Jaguar(RobotMap.driveMotorLFPort),
+                        new Jaguar(RobotMap.driveMotorLRPort),
+                        new Jaguar(RobotMap.driveMotorRFPort),
+                        new Jaguar(RobotMap.driveMotorRRPort) };
 
-    // drive_simple START
-    private CANTalon motor1 = new CANTalon(RobotMap.liftMotorCanId);
-    private CANTalon motor2 = new CANTalon(RobotMap.liftMotorCanId + 1);
-    private CANTalon motor3 = new CANTalon(RobotMap.liftMotorCanId + 2);
-    private CANTalon motor4 = new CANTalon(RobotMap.liftMotorCanId + 3);
-    // drive_simple END
-
-    private Drive drive = new Drive(motor1, motor2, motor3, motor4);
+    private Drive drive = new Drive(motors[0], motors[1], motors[2], motors[3]);
 
     public void initDefaultCommand() {
         setDefaultCommand(new DriveTeleopCommand());
     }
 
     public void init() {
-        // drive_simple START
-        motor1.enableControl();
-        motor1.changeControlMode(ControlMode.PercentVbus);
-        motor1.setSafetyEnabled(true);
-        motor1.setExpiration(0.100);
-        motor1.set(0.0);
-        // drive_simple END
+        for (Jaguar motor : motors) { init(motor); }
+    }
+
+    public void enable() {
+        for (Jaguar motor : motors) { enable(motor); }
+    }
+
+    private void init(Jaguar motor) {
+        enable(motor);
+        motor.set(0.0);
+    }
+
+    private void enable(Jaguar motor) {
+        motor.Feed();   // motor.enableControl();
+        motor.setSafetyEnabled(true);
+        motor.setExpiration(0.100);
     }
 
     public Drive.Mode getMode() {
