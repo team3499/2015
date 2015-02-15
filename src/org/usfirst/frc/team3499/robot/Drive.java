a73f0c7c018a46ac944d6672fd201f70be1adb07
bturnbull <github@brianturnbull.com>
2015-02-15 12:59:53 -0500

    Add voltage ramp to Drive system
    Adds a (source code) configurable voltage ramp to the Drive system
    for the chassis motors.

--- src/org/usfirst/frc/team3499/robot/Drive.java
+++ src/org/usfirst/frc/team3499/robot/Drive.java
@@ -1,7 +1,10 @@
 package org.usfirst.frc.team3499.robot;
 
+import java.lang.Math;
+
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Timer;
 
 /**
  * Extends RobotDrive to allow crawl and ramp control
@@ -16,16 +19,25 @@ public class Drive extends RobotDrive {
     };
 
     public static Mode mode = Mode.NORMAL;
-    public double crawlPercent = 0.3;
+    public static double crawlPercent = 0.3;     // percentage of full speed
+    public static double rampThrottle = 0.02;    // max change per 10ms
+    private static Timer timer = new Timer();
+
+    private double rampedLastSampleTime;
+    private double rampedLeftOutput;
+    private double rampedRightOutput;
 
     public Drive(SpeedController frontLeftMotor,
                  SpeedController rearLeftMotor,
                  SpeedController frontRightMotor,
                  SpeedController rearRightMotor) {
         super(frontLeftMotor, rearLeftMotor, frontRightMotor, rearRightMotor);
+        timer.start();
+        updateRampSample(0.0, 0.0);
     }
 
     public void setLeftRightMotorOutputs(double leftOutput, double rightOutput) {
+
         switch (mode) {
             case NORMAL:
                 break;
@@ -37,9 +49,40 @@ public class Drive extends RobotDrive {
                 // TODO
                 break;
             case RAW:
-                // TODO
-                break;
+                updateRampSample(leftOutput, rightOutput);
+                super.setLeftRightMotorOutputs(leftOutput, rightOutput);
+                return;
         }
+
+        setRampedLeftRightMotorOutputs(leftOutput, rightOutput);
+    }
+
+    // When not in RAW mode, only allow changes of rampThrottle per 10ms in the
+    // value of motor output
+    public void setRampedLeftRightMotorOutputs(double leftOutput, double rightOutput) {
+        double now = timer.get();
+        double maxChange = Math.min((now - rampedLastSampleTime) * 100 * rampThrottle, 1.0);
+
+        if (Math.abs(leftOutput - rampedLeftOutput) > maxChange) {
+            if (leftOutput < rampedLeftOutput) { leftOutput = rampedLeftOutput - maxChange; }
+            else { leftOutput = rampedLeftOutput + maxChange; }
+        }
+        if (Math.abs(rightOutput - rampedRightOutput) > maxChange) {
+            if (rightOutput < rampedRightOutput) { rightOutput = rampedRightOutput - maxChange; }
+            else { rightOutput = rampedRightOutput + maxChange; }
+        }
+
+        updateRampSample(now, leftOutput, rightOutput);
         super.setLeftRightMotorOutputs(leftOutput, rightOutput);
+    }
+
+    private void updateRampSample(double leftOutput, double rightOutput) {
+        updateRampSample(timer.get(), leftOutput, rightOutput);
+    }
+
+    private void updateRampSample(double time, double leftOutput, double rightOutput) {
+        rampedLastSampleTime = time;
+        rampedLeftOutput     = leftOutput;
+        rampedRightOutput    = rightOutput;
     }
 }
